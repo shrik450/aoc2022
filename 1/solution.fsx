0ac5767bd27bfd6d363782e39bfb86eecade6b38
Shrikanth Upadhayaya <shrik450@gmail.com>
2022-12-01 09:29:39 -0500

    Commented + those annoying hints are gone

--- 1/solution.fsx
+++ 1/solution.fsx
@@ -2,7 +2,16 @@ open System.IO
 
 let input = File.ReadLines "./input"
 
-let chunk totalCalories currentCalories =
+/// The line parsed as an integer, with a blank line becoming -1.
+///
+/// This allows for chunking by value without having to use any mutable values
+/// for indexing, as in the answers of
+/// https://stackoverflow.com/questions/6736464/split-seq-in-f
+let parsed (line: string) =
+    if line.Length = 0 then -1 else int line
+
+/// The list of total calories carried by each reindeer.
+let totals totalCalories currentCalories =
     if currentCalories = -1 then
         0 :: totalCalories
     else
@@ -10,16 +19,15 @@ let chunk totalCalories currentCalories =
         | head :: tail -> head + currentCalories :: tail
         | _ -> failwith "This isn't supposed to happen."
 
+// The two result values are functions so the file is only read when called.
+
 let result_first () =
-    input
-    |> Seq.map (fun line -> if line.Length = 0 then -1 else int line)
-    |> Seq.fold chunk [ 0 ]
-    |> List.max
+    input |> Seq.map parsed |> Seq.fold totals [ 0 ] |> List.max
 
 let result_second () =
     input
-    |> Seq.map (fun line -> if line.Length = 0 then -1 else int line)
-    |> Seq.fold chunk [ 0 ]
+    |> Seq.map parsed
+    |> Seq.fold totals [ 0 ]
     |> List.sortDescending
     |> List.take 3
     |> List.sum
